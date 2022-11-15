fa2d66827c23ea647a465ce009cd746d4ba79cc9
pholaneckes <114641324+pholaneckes@users.noreply.github.com>
2022-11-15 18:29:28 +0800

    Add files via upload

---
+++ src/main/java/xiaoleqwq/formula_addition/Formula_Addition.java
@@ -0,0 +1,10 @@
+package xiaoleqwq.formula_addition;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("formula_addition")
+public class Formula_Addition {
+    public Formula_Addition() {
+
+    }
+}
